1165cb51d25d553dc60ae39093c20c494a8757fb
Gaurav Saluja <gauravsaluja2006@gmail.com>
2020-08-24 18:58:52 +0530

    Updates

---
+++ precache-manifest.43f7020aee2d115cd9dbcdfbcfb3d206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2123ac1b0078a8703eba61bd962630",
+    "url": "/text-to-speech-playground/index.html"
+  },
+  {
+    "revision": "2d6d7a6c216b5edbf852",
+    "url": "/text-to-speech-playground/static/css/main.70931c9a.chunk.css"
+  },
+  {
+    "revision": "4ae69b51aada3cef5cbb",
+    "url": "/text-to-speech-playground/static/js/2.5c47d274.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/text-to-speech-playground/static/js/2.5c47d274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6d7a6c216b5edbf852",
+    "url": "/text-to-speech-playground/static/js/main.c979304f.chunk.js"
+  },
+  {
+    "revision": "38b4007d2d4f9716cb21",
+    "url": "/text-to-speech-playground/static/js/runtime-main.51d2b9dd.js"
+  }
+]);
